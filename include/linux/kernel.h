9588997258f241d4b5b4f573d64009f74fa9797e
Mohd Faraz <mohd.faraz.abc@gmail.com>
2018-07-30 12:33:58 +0000

    kernel.h: Fix a error caused while cherry-picking
    Signed-off-by: Mohd Faraz <mohd.faraz.abc@gmail.com>

--- include/linux/kernel.h
+++ include/linux/kernel.h
@@ -805,4 +805,4 @@ static inline void ftrace_dump(enum ftrace_dump_mode oops_dump_mode) { }
 /* To identify board information in panic logs, set this */
 extern char *mach_panic_string;
 
-endif
+#endif
